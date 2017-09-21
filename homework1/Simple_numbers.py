7b8f212034075eefbb940c4d3a1a396d2f22bb05
Evgeniy Babadjanov <jeeugenebbj@gmail.com>
2017-09-21 16:56:37 +0300

    Update Simple_numbers.py

--- homework1/Simple_numbers.py
+++ homework1/Simple_numbers.py
@@ -1,3 +1,9 @@
-a = int(input('a:'))
-for i in range(a):
-    print (i+1)
+a = input('a:')
+lst = []
+for i in range(2, a+1):
+    for j in range(2, i):
+        if i % j == 0:
+            break
+    else:
+        lst.append(i)
+print lst
